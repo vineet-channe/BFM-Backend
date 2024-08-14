7f3bda8ad32a1ae1bad782c1ad8902a947f60649
vineet-channe <vineetchanneproffesional@gmail.com>
2024-08-15 02:33:46 +0530

    Removed Comments

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const rootRouter = require("./routes/index");
-// const app = express();
-// app.use("/api",rootRouter);
-
 import express from "express";
 import rootRouter from "./routes/index";
 const app = express();
