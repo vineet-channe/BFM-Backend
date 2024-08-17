b8a0cd57c1139711e2c26363b6edf3fcb6e4b69e
vineet-channe <vineetchanneproffesional@gmail.com>
2024-08-17 14:24:36 +0530

    Added PORT in .env

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import rootRouter from "./routes/index";
+import dotenv from "dotenv";
+dotenv.config({path : '../config/.env'});
 const app = express();
 const router = express.Router();
 app.use("/api",rootRouter);
+app.listen(process.env.PORT,() => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
