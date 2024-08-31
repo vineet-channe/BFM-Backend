915069602d75ecf5395a5f690dcb41f20c4e5b31
vineet-channe <vineetchanneproffesional@gmail.com>
2024-08-31 20:57:22 +0530

    CORS

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 import rootRouter from "./routes/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const router = express.Router();
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://metaverse-marketplace-frontend.vercel.app/']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api",rootRouter);
